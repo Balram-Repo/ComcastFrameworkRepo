4b0b5e609687d18e95ab0a85d4e563dd5e4b058c
Balram-Repo <cbalaram121@gmail.com>
2025-11-07 12:02:05 +0530

    Added new POM class- DocumentsPage

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/DocumentsPage.java
@@ -0,0 +1,21 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DocumentsPage {
+
+	public DocumentsPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath = "//img[@title='Create Document...']")
+	private WebElement createDocBtn;
+
+	public WebElement getCreateDocBtn() {
+		return createDocBtn;
+	}
+	
+}
